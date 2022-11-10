5626aafc5bec024c39a506d8da58413d35127e39
Agnusse <116185310+Agnusse@users.noreply.github.com>
2022-11-10 20:26:48 +0100

    Hausaufgabe_04 roboter mit goal und decoys

--- Assets/Script/Decoy/TriggerDecoy.cs
+++ Assets/Script/Decoy/TriggerDecoy.cs
@@ -2,28 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerGoal : MonoBehaviour
+public class TriggerDecoy : MonoBehaviour
 {
     public GameObject Roboter;
 
     AudioSource audioData;
-    
+
     private void OnTriggerEnter (Collider other)
     {
         Debug.Log (other.name + " just hit " + gameObject.name);
 
         if(other.name == Roboter.name)
         {
-            // when roboter collides with goal
-            Debug.Log("Victory!!!!");
-
+            // when roboter collides with decoy
+            Debug.Log("Decoy was hit");
 
             audioData = GetComponent<AudioSource>();
             audioData.Play(0);
             Debug.Log("started");
 
+            Destroy(gameObject, 1);
         }
-
-  
     }
 }
--- Assets/Script/Dice/dice.cs
+++ Assets/Script/Dice/dice.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dice : MonoBehaviour
+public class Dice : MonoBehaviour
 {
+    //Variables
+    public int diceNumber;
+    
+    //winning numbers
+    public int firstWin = 1;
+    public int secondWin = 3;
+    public int thirdWin = 6;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space")) {
+            diceNumber = Random.Range(1, 7);
+            Debug.Log("Dice Number: " + diceNumber);
+            
+            if (diceNumber == firstWin ) {
+                Debug.Log ( "You won with the lucky number  "  + firstWin);
+            }
+            else if (diceNumber == secondWin){
+                Debug.Log ( "You won with the lucky number  " + secondWin);
+            }
+            else if (diceNumber == thirdWin){
+                Debug.Log ( "You won with the lucky number  " + thirdWin);
+            }
+            else {
+                Debug.Log ("You lost");
+            }
+        }
         
     }
 }
