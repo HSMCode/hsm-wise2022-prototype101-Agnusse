101e0be40065d3421300fb0c2e092bb1645b5a60
Agnuss <agnes.stigler@students.hs-mainz.de>
2022-11-18 11:02:59 +0100

    Dice nur noch eine Ausgabe ob gewonnen oder verloren

--- Assets/Script/Dice/dice.cs
+++ Assets/Script/Dice/dice.cs
@@ -11,6 +11,9 @@ public class Dice : MonoBehaviour
     public int diceNumber;
     public int [] luckyNumbers ={1,10,11,21,22,31,33,44};
 
+    //initialize win bool
+    public bool luckyNumberWasDrawn = false;
+
     // Update is called once per frame
     void Update()
     {
@@ -18,25 +21,25 @@ public class Dice : MonoBehaviour
         if (Input.GetKeyDown("space")) {
             diceNumber = Random.Range(1, 51);
 
-            Debug.Log ("Dice number " + diceNumber); 
-
             //check again until i=0; so all of the numbers in the array
             for (int i = 0; i < luckyNumbers.Length ; i++)
             {
             // then do this
               // if lucky number is drawn
               if (diceNumber == luckyNumbers[i])  {
-                 Debug.Log ( " Congratulations!!! You won with the lucky number  "  + diceNumber + " in loop " + i);
+                 Debug.Log ( " Congratulations!!! You won with the lucky number  "  + diceNumber);
+                 luckyNumberWasDrawn = true;
                  // play audio
                  audioData = GetComponent<AudioSource>();
                  audioData.Play(0);
 
               }
               //otherwise you lost
-              else {
-                  Debug.Log ("Sorry! You lost with the number " + diceNumber + " in loop " + i);
+              else if (i == luckyNumbers.Length-1 && luckyNumberWasDrawn == false) {
+                  Debug.Log ("Sorry! You lost with the number " + diceNumber );
               }
             }
+            luckyNumberWasDrawn = false;
         }
     }
 }
