db245012650688c0bf158d4e8d2a47d087f81e85
Agnusse <116185310+Agnusse@users.noreply.github.com>
2022-12-15 16:19:58 +0100

    Sprung überarbeitet, Enemy hinzu gefügt und Spawn Enemy

---
+++ Assets/MoveEnemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+
+//  {
+ 
+//      public Transform Player;
+//      int MoveSpeed = 4;
+
+//      void Start()
+//      {
+ 
+//      }
+ 
+//      void Update()
+//      {
+//          transform.LookAt(Player);
+ 
+//         transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+ 
+    
+//      }
+//  }
+
+{
+
+    public Transform target;
+    public Transform myTransform;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt (target);
+        transform.Translate(Vector3.forward*5*Time.deltaTime);
+    }
+}
+
